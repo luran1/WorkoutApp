7351947ba8c43e3a8ba24edea24ba6b50b18e9ba
luran1 <luranmanfio0701@gmail.com>
2025-02-06 20:51:14 -0500

    Template project started.

---
+++ Workout/Workout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout
+//
+//  Created by Madison Francis on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout/Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by Madison Francis on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
